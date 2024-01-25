ec00bbfdb2ff97b1b4f1dc677c2812535f948be6
Ryan Steckler <gotcode@gmail.com>
2024-01-25 12:24:50 -0800

    font and real answer in node

--- node/server.js
+++ node/server.js
@@ -6,6 +6,10 @@ const port = 3000;
 app.post('/index', cors(), (req, res) => {
     console.log(`got a request`);
 
+    console.log("data is: " + req.body);
+    const answer = req.body.textbox;
+    console.log("anwqer is: " + answer);
+
     // Handle the POST request here
     res.send('RFC1035-3.3.14');
 });
