97ba3c924b64db5f6452190befd7031d5b62a3ac
Ryan Steckler <gotcode@gmail.com>
2024-01-25 12:30:55 -0800

    server url encoding

--- node/server.js
+++ node/server.js
@@ -6,13 +6,15 @@ const port = 3000;
 app.post('/index', cors(), (req, res) => {
     console.log(`got a request`);
 
-    console.log("data is: " + req.body);
+    console.log("data is: " + req.);
     const answer = req.body.textbox;
     console.log("anwqer is: " + answer);
 
     // Handle the POST request here
     res.send('RFC1035-3.3.14');
 });
+
+app.use(require('body-parser').urlencoded({ extended: false }));
 
 app.use(
     cors({
