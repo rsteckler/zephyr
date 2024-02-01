ec99b06a4219bbdab0e92f2f51d4506dc8b027fa
Ryan Steckler <gotcode@gmail.com>
2024-01-31 19:04:12 -0800

    autoplay video

---
+++ node/awsconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aws_table_name: 'leaderboard',
+    aws_local_config: {
+      //Provide details for local configuration
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIA4UNPD7EGSL4S2IKK',
+      secretAccessKey: '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',
+      region: 'us-east-1',
+    }
+};
