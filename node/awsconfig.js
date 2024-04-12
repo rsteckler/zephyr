9eef2976dcce5e48f851d234e191a277a4549ea5
Ryan Steckler <gotcode@gmail.com>
2024-04-12 13:48:57 -0700

    revoke IAM key

--- node/awsconfig.js
+++ node/awsconfig.js
@@ -4,8 +4,8 @@ module.exports = {
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: 'AKIA4UNPD7EGSL4S2IKK',
-      secretAccessKey: '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',
+      accessKeyId: 'revoked',
+      secretAccessKey: 'revoked',
       region: 'us-east-1',
     }
 };
